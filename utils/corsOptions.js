acfb211e6e8f0b8ea845986624f54211c35f8b81
Kamit6337 <kamit6337@gmail.com>
2024-09-28 18:49:40 +0530

    update cors option

--- utils/corsOptions.js
+++ utils/corsOptions.js
@@ -1,5 +1,5 @@
 export const corsOptions = {
-  origin: ["http://localhost:5173"],
+  origin: ["https://send-it-client.onrender.com", "http://localhost:5173"],
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
   credentials: true,
 };
